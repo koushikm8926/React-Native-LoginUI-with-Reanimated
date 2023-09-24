f69d5a3d0e59ab29a17fb6041772d539b8874912
Koushik Mondal <darkdeavil004@gmail.com>
2023-09-24 11:42:35 +0530

    navigation added

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View className="bg-red-500">
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen}/>
+      </Stack.Navigator>
+  </NavigationContainer>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
